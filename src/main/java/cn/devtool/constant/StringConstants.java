d282a8249d81592d6040eb57571b5bd032a93902
Yujiumin <934639947@qq.com>
2022-04-09 19:30:16 +0800

    字符串常量

---
+++ src/main/java/cn/devtool/constant/StringConstants.java
@@ -0,0 +1,50 @@
+package cn.devtool.constant;
+
+/**
+ * 字符串常量
+ *
+ * @author Yujiumin
+ * @version 2021/08/17
+ */
+public class StringConstants {
+
+    /**
+     * windows系统下的文件分隔符
+     */
+    public static final String FILE_SEPARATOR_WINDOWS = "\\";
+
+    /**
+     * linux系统下的文件分隔符
+     */
+    public static final String FILE_SEPARATOR_LINUX = "/";
+
+    /**
+     * 空字符[]
+     */
+    public static final String EMPTY = "\u0000";
+
+    /**
+     * 空格[ ]
+     */
+    public static final String SPACE = "\u0020";
+
+    /**
+     * 逗号[,]
+     */
+    public static final String COMMA = "\u002C";
+
+    /**
+     * 中划线[-]
+     */
+    public static final String HYPHEN_MINUS = "\u002D";
+
+    /**
+     * 下划线[_]
+     */
+    public static final String LOW_LINE = "\u005F";
+
+    /**
+     * 空字符串[]
+     */
+    public static final String EMPTY_STRING = EMPTY.trim();
+}
