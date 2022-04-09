5eb5110598f41981acc7947f82a49034b7f17f8c
Yujiumin <934639947@qq.com>
2022-04-09 19:17:55 +0800

    创建项目

---
+++ src/main/java/cn/devtool/Devtool.java
@@ -0,0 +1,15 @@
+package cn.devtool;
+
+/**
+ * @author Yujiumin
+ * @date 2022/04/09
+ */
+public class Devtool {
+
+    private static final String AUTHOR_NAME = "Yujiumin";
+    private static final String AUTHOR_EMAIL = "jiumin.yu@foxmail.com";
+
+    public static void main(String[] args) {
+
+    }
+}
